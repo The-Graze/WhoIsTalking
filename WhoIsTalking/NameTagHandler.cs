394dbcfee5738002a1ca3bedc5e88e8cbd609741
Graze <82724623+The-Graze@users.noreply.github.com>
2024-02-08 12:08:52 +0000

    Complete Rewirte

---
+++ WhoIsTalking/NameTagHandler.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using Photon.Realtime;
+using Photon.Voice.PUN;
+
+namespace WhoIsTalking
+{
+    class NameTagHandler : MonoBehaviour
+    {
+        GameObject NameFP, NameTP;
+        Renderer FPRend, TPRend, FPSpeakerRend, TPSpeakerRend;
+        TextMesh FPText, TPText;
+
+        VRRig rig;
+        Player player;
+        PhotonVoiceView voice;
+
+        Color Orange = new Color(1, 0.3288f, 0, 1);
+
+        void Start()
+        {
+            SetUpNameTag();
+        }
+        void OnEnable()
+        {
+            GetInfo();
+        }
+        void SetUpNameTag()
+        {
+            SetUpNameTagInstance(ref NameFP, "First Person NameTag", "GorillaCosmeticParticle");
+            FPSpeakerRend = NameFP.transform.GetChild(0).GetComponent<Renderer>();
+            FPSpeakerRend.material.shader = AssetRef.shader;
+            FPRend = NameFP.transform.GetChild(1).GetComponent<Renderer>();
+            FPRend.material.shader = AssetRef.shader;   
+            FPText = FPRend.GetComponent<TextMesh>();
+            FPSpeakerRend.gameObject.AddComponent<Spinner>().Speed = 1f;
+
+            SetUpNameTagInstance(ref NameTP, "Third Person NameTag", "Gorilla Spectator");
+            TPSpeakerRend = NameTP.transform.GetChild(0).GetComponent<Renderer>();
+            TPSpeakerRend.material.shader = AssetRef.shader;
+            TPRend = NameTP.transform.GetChild(1).GetComponent<Renderer>();
+            TPRend.material.shader = AssetRef.shader;
+            TPText = TPRend.GetComponent<TextMesh>();
+            TPSpeakerRend.gameObject.AddComponent<Spinner>().Speed = 1f;
+        }
+
+        void SetUpNameTagInstance(ref GameObject nameTag, string name, string layerName)
+        {
+            nameTag = Instantiate(AssetRef.Tag, transform);
+            nameTag.transform.localPosition = new Vector3(0f, -1.727f, 0f);
+            nameTag.layer = LayerMask.NameToLayer(layerName);
+            foreach (Transform t in nameTag.transform)
+            {
+                t.gameObject.layer = LayerMask.NameToLayer(layerName);
+            }
+            nameTag.name = name;
+        }
+
+        void GetInfo()
+        {
+            rig = GetComponent<VRRig>();
+            player = rig.creator;
+            voice = VRRigCache.rigsInUse[player].photonVoiceView;
+        }
+
+        void Update()
+        {
+            try
+            {
+                FirstPersonViewDistance();
+                Color color = ColourHandling();
+                FPRend.material.color = color;
+                FPSpeakerRend.material.color = color;
+                FPRend.transform.LookAt(Camera.main.transform.position);
+
+                TPSpeakerRend.material.color = color;
+                TPRend.material.color = color;
+                TPRend.transform.LookAt(GorillaTagger.Instance.thirdPersonCamera.transform.position);
+
+
+                TPSpeakerRend.forceRenderingOff = !voice.IsSpeaking;
+                TPText.text = player.NickName;
+                FPText.text = player.NickName;
+            }
+            catch
+            {
+                GetInfo();
+            }
+        }
+
+        void FirstPersonViewDistance()
+        {
+            float Distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+            float viewDistance = 4f;
+            bool CanSee = (bool)(Distance <= viewDistance);
+            FPSpeakerRend.forceRenderingOff = !voice.IsSpeaking || !CanSee;
+            FPRend.forceRenderingOff = !CanSee;
+        }
+
+        Color ColourHandling()
+        {
+            switch (rig.setMatIndex)
+            {
+                default:
+                    return new Color(
+                        rig.materialsToChangeTo[rig.tempMatIndex].color.r,
+                        rig.materialsToChangeTo[rig.tempMatIndex].color.g,
+                        rig.materialsToChangeTo[rig.tempMatIndex].color.b,
+                        1
+                    );
+                case 2:
+                case 11:
+                    return Orange;
+                case 3:
+                case 7:
+                    return Color.blue;
+                case 12:
+                    return Color.green;
+            }
+        }
+    }
+}
--- WhoIsTalking/Plugin.cs
+++ WhoIsTalking/Plugin.cs
@@ -6,163 +6,30 @@ using System.IO;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using WhoIsTalking.Patches;
 
 namespace WhoIsTalking
 {
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        public GameObject speaker;
-        public static volatile Plugin Instance;
-        public Shader shader;
-        public float viewDistance = 3.5f;
-        void Start()
+        public Plugin()
         {
-            Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("WhoIsTalking.Assets.speaker");
-            AssetBundle bundle = AssetBundle.LoadFromStream(str);
-            GameObject bweep = bundle.LoadAsset<GameObject>("speaker");
-            speaker = bweep;
-            speaker.name = "NameTagStore";
-            Instance = this;
-            Utilla.Events.GameInitialized += OnGameInitialized;
-            Config.Bind("Settings", "Camera Look Target", false);
-            HarmonyPatches.ApplyHarmonyPatches();
+            Utilla.Events.GameInitialized += GameInit;
         }
-        void Update()
+        private void GameInit(object sender, EventArgs e)
         {
-            viewDistance = 3.5f;
-        }
-        void OnGameInitialized(object sender, EventArgs e)
-        {
-            shader = GameObject.Find("motdtext").GetComponent<Text>().material.shader;
-        }
-    }
-    class Talkies : MonoBehaviour
-    {
-        GameObject LoadSpeaker;
-        GameObject LoadSpeaker2;
-        GameObject LSpeaker;
-        GameObject Speaker2;
-        GameObject NameTag;
-        GameObject NameTag2;
-        Renderer SpeakerRend;
-        Renderer SpeakerRend2;
-        Renderer NameRend;
-        Renderer NameRend2;
-        public PhotonVoiceView voice;
-        public PhotonView view;
-        public VRRig rig;
-        Plugin p = Plugin.Instance;
-        TextMesh nametagname;
-        TextMesh nametagname2;
-        Transform Lookat;
-        Transform Lookat2;
-        Spinner spwinner;
-        Spinner spwinner2;
-        Color Orange;
-        float Distance;
-        void Start()
-        {
-            if (rig.isOfflineVRRig)
+            AssetRef.shader = GameObject.Find("motdtext").GetComponent<Text>().material.shader;
+            foreach (Transform t in GameObject.Find("Player Objects/RigCache/Rig Parent").transform)
             {
-                Destroy(this);
+                if (t.GetComponent<VRRig>())
+                {
+                    t.gameObject.AddComponent<NameTagHandler>();
+                }
             }
-            else
-            {
-                LoadSpeaker = Instantiate(p.speaker, transform);
-                LoadSpeaker2 = Instantiate(p.speaker, transform);
-                LoadSpeaker.transform.localPosition = new Vector3(0f, -1.727f, 0f);
-                LoadSpeaker2.transform.localPosition = new Vector3(0f, -1.727f, 0f);
-                LoadSpeaker.name = "fpPlayerNameTag";
-                LoadSpeaker2.name = "tpPlayerNameTag";
-                LoadSpeaker.layer = LayerMask.NameToLayer("GorillaCosmeticParticle");
-                LoadSpeaker2.layer = LayerMask.NameToLayer("Gorilla Spectator");
-                LSpeaker = LoadSpeaker.transform.GetChild(0).gameObject;
-                LSpeaker.layer = LayerMask.NameToLayer("GorillaCosmeticParticle");
-                NameTag = LoadSpeaker.transform.GetChild(1).gameObject;
-                NameTag.layer = LayerMask.NameToLayer("GorillaCosmeticParticle");
-                NameTag2 = LoadSpeaker2.transform.GetChild(1).gameObject;
-                NameTag2.layer = LayerMask.NameToLayer("Gorilla Spectator");
-                Speaker2 = LoadSpeaker2.transform.GetChild(0).gameObject;
-                Speaker2.layer = LayerMask.NameToLayer("Gorilla Spectator");
-                nametagname = NameTag.GetComponent<TextMesh>();
-                nametagname2 = NameTag2.GetComponent<TextMesh>();
-                SpeakerRend = LSpeaker.GetComponent<Renderer>();
-                SpeakerRend2 = Speaker2.GetComponent<Renderer>();
-                NameRend = NameTag.GetComponent<Renderer>();
-                NameRend2 = NameTag2.GetComponent<Renderer>();
-                SpeakerRend.material.shader = p.shader;
-                SpeakerRend2.material.shader = p.shader;
-                NameRend.material.shader = p.shader;
-                NameRend2.material.shader = p.shader;
-                nametagname = NameTag.GetComponent<TextMesh>();
-                spwinner = LSpeaker.AddComponent<Spinner>();
-                spwinner2 = Speaker2.AddComponent<Spinner>();
-                spwinner.Speed = 1;
-                spwinner2.Speed = 1;
-                Orange = new Color(1, 0.3288f, 0, 1);
-                Lookat = Camera.main.transform;
-                Lookat2 = Camera.allCameras[1].transform;
-            }
-        }
-        void LateUpdate()
-        {
-            nametagname.text = view.Controller.NickName;
-            nametagname2.text = view.Controller.NickName;
-            NameTag.transform.LookAt(Lookat);
-            NameTag2.transform.LookAt(Lookat2);
-            Distance = Vector3.Distance(transform.position, Camera.main.transform.position);
-            Cansee();
-            Plugin.Instance.viewDistance = 3.5f;
-            SpeakerRend2.enabled = voice.IsSpeaking || voice.IsRecording;
-            SpeakerRend2.material.color = Pcol();
-            NameRend2.material.color = Pcol();
-        }
-        bool Cansee()
-        {
-            if (Distance <= Plugin.Instance.viewDistance)
-            {
-                NameRend.enabled = true;
-                SpeakerRend.enabled = voice.IsSpeaking || voice.IsRecording;
-                SpeakerRend.material.color = Pcol();
-                NameRend.material.color = Pcol();
-                return true;
-            }
-            else
-            {
-                SpeakerRend.enabled = false;
-                NameRend.enabled = false;
-                return false;
-            }
-        }
-        public Color Pcol() 
-        {
-            Color temp = new Color();
-            if (rig.setMatIndex == 2)
-            {
-                return Orange;
-            }
-            if (rig.setMatIndex == 3)
-            {
-                return Color.blue;
-            }
-            if (rig.setMatIndex == 7)
-            {
-                return Color.blue;
-            }
-            if (rig.setMatIndex == 11)
-            {
-                return Orange;
-            }
-            else
-            {
-                temp.r = rig.materialsToChangeTo[rig.tempMatIndex].color.r;
-                temp.g = rig.materialsToChangeTo[rig.tempMatIndex].color.g;
-                temp.b = rig.materialsToChangeTo[rig.tempMatIndex].color.b;
-                temp.a = 1;
-                return temp;
-            }
+            AssetRef.shader = GameObject.Find("motdtext").GetComponent<Text>().material.shader;
         }
     }
 }
