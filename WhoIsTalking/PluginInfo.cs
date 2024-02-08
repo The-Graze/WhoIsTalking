4e0baea0642ad70f683db536ac5cb0b14ed64393
Graze <82724623+The-Graze@users.noreply.github.com>
2024-02-08 12:24:05 +0000

    forgot plugin info agan

--- WhoIsTalking/PluginInfo.cs
+++ WhoIsTalking/PluginInfo.cs
@@ -1,12 +1,9 @@
 ﻿namespace WhoIsTalking
 {
-    /// <summary>
-    /// This class is used to provide information about your mod to BepInEx.
-    /// </summary>
     internal class PluginInfo
     {
         public const string GUID = "com.graze.gorillatag.whoistalking";
         public const string Name = "WhoIsTalking";
-        public const string Version = "3.7.2";
+        public const string Version = "4.0.0";
     }
 }
